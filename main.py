13293d28bfc99156846baa2461011304fa4d40a5
gwarzecha <gmwarzecha@tutanota.com>
2024-09-17 17:17:01 -0700

    Add containers to handle updates and drawing in the game loop

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()    
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -14,9 +19,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
